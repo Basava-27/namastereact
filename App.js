40004f44cceb2c79bf6ea05eb622e6802f6b4bae
basavsp27 <basava504212345@gmail.com>
2025-07-18 01:16:17 +0530

    episode 04 till 34 min

--- App.js
+++ App.js
@@ -1,35 +1,99 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+/**
+ * Header
+ * - logo
+ * - Nav Items
+ * body
+ * -search
+ * - restaurant container
+ *  - testaurant card
+ * footer
+ * - copyright
+ * - address
+ * - contact
+ */
 
-const elem=<span>React element</span>
+const Header=()=>{
+  return (
+    <div className="header">
+      <div>
+        <img 
+        className="logo"
+        src="https://icon-library.com/images/food-app-icon/food-app-icon-5.jpg"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const Title=()=>(
-  <h1>Hello</h1>
-);
 
-const heading=(
-  <div>
-    <Title/>
-    {Title()}
-    <Title></Title>
-    {180+100}
-    <h1 id="heading">
-      {elem}
-      Namaste React
-    </h1>
+
+const RestaurantCard=()=>{
+  return (
+    <div className="res-card">
+      <img 
+      className="res-logo" 
+      alt="press logo" 
+      src="https://b.zmtcdn.com/data/o2_assets/110a09a9d81f0e5305041c1b507d0f391743058910.png"/>
+      <h3>Meghana Foods</h3>
+      <h4>Burger,Western,American</h4>
+      <h4>4.4 stars</h4>
+      <h4>36 minutes</h4>
+    </div>
+  );
+}
+
+const Body=()=>{
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+      </div>
+    </div>
+  );
+}
+
+
+const AppLayout=()=>{
+  return (
+  <div className="app">
+    <Header/>
+    <Body/>
   </div>
-);
+  );
+};
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
 
 
-const Headingcomponent=()=>(
-  <div>
-    {heading}
-    <Title />
-    <h1 id="heading">Namaste react from component</h1>
-  </div>
-);
-
-
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<Headingcomponent/>);
+ 
